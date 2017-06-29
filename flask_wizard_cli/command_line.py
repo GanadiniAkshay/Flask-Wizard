6cf7ce0ae6179af3b2d1b2ea60fbe91c0ec30fef
Akshay <akshaykulkarni.2104@gmail.com>
2017-06-29 16:52:07 +0530

    fixed the learn bug

--- flask_wizard_cli/command_line.py
+++ flask_wizard_cli/command_line.py
@@ -56,13 +56,6 @@ def build(arguments):
     file.close()
 
 def learn(arguments):
-    config = os.path.join(os.getcwd(),'config.json')
-    with open(config,"r") as jsonFile:
-                data = json.load(jsonFile)
-                if data["nlp"] == "rasa":
-                    from rasa_nlu.converters import load_data
-                    from rasa_nlu.config import RasaNLUConfig
-                    from rasa_nlu.model import Trainer
     path = os.path.join(os.getcwd(), 'train')
     if (os.path.isdir(path)):
         ac_path = os.path.join(os.getcwd(),'actions')
@@ -206,43 +199,83 @@ def init(arguments):
     if len(user_name) == 0:
         user_name = "wizard_user"
 
-    #Ask if ozz account
-    print("Your ozz account is used for accessing the GUI admin panel")
+    print("\n\nYour ozz account is used for accessing the GUI admin panel")
     print("The admin panel lets you add/remove channels, access analytics and other services")
     answer = str(input("Do you have an account on Ozz.ai?[Y/N]:"))
 
     answer = answer.lower()
+    flag = True
 
-    if answer == 'y' or answer == 'yes' or answer == 'yeah' or answer == 'yup':
-        email = str(input("Email:"))
-        password = str(input("Password:"))
-    else:
-        #Setup Email
-        prompt = "Email for admin panel:"
-        email = str(input(prompt))
-        #Setup Password
-        prompt = "Password for admin panel (typing hidden):"
-        password = str(getpass.getpass(prompt))
+    while flag:
+        if answer == 'y' or answer == 'yes' or answer == 'yeah' or answer == 'yup':
+            email = str(input("Email:"))
+            password = str(getpass.getpass('Password:'))
 
-        url = "https://api.ozz.ai/users"
-        headers = {
-        'content-type': "application/json",
-        'cache-control': "no-cache"
-        }
-        payload = {
-            "name":user_name,
-            "email":email,
-            "password":password
-        }
-        print("Setting up admin account....")
-        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+            url = "https://api.ozz.ai/auth"
+            headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+            }
+            payload = {
+                "email":email,
+                "password":password
+            }
+            
+            response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
 
-        response_token = json.loads(response.text)['token']
-        super_secret = "hdh%&D*%^^%$8767VHGSDFT5$%SD$658"
+            response = json.loads(response.text)
 
-        data = jwt.decode(response_token,super_secret,algorithms=['HS256'])
+            if 'email' in response:
+                print("\nNo account exists with that email. Try again")
+            elif 'password' in response:
+                print("\nInvalid password. Try Again")
+            elif 'success' in response and response['success'] == 'true':
+                flag = False
+        else:
+            #Setup Email
+            prompt = "Email for admin panel:"
+            email = str(input(prompt))
+            #Setup Password
+            prompt = "Password for admin panel (typing hidden):"
+            password = str(getpass.getpass(prompt))
 
-        user_id = data["id"]
+            url = "https://api.ozz.ai/users"
+            headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+            }
+            payload = {
+                "name":user_name,
+                "email":email,
+                "password":password
+            }
+            print("Setting up admin account....")
+            response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+
+            response = json.loads(response.text)
+
+            if 'email' in response:
+                print("\n\nAn email with that account already exists")
+                
+                while(True):
+                    result = input('Do you want to use the existing account or create a new one? \n 1) Use Existing 2) Create New \n Please choose 1 or 2:')
+                    try:
+                        result = int(result)
+                    except ValueError:
+                        continue
+                    if result == 1:
+                        flag = False
+                        break
+                    else:
+                        break
+            else:
+                flag = False
+                response_token = response['token']
+                super_secret = "hdh%&D*%^^%$8767VHGSDFT5$%SD$658"
+
+                data = jwt.decode(response_token,super_secret,algorithms=['HS256'])
+
+                user_id = data["id"]
 
 
     #Setup actions folder
--- test.py
+++ test.py
@@ -24,66 +24,83 @@ import os
 # print(user_id)
 
 #Setup bot name
-arguments = []
-main_name = os.getcwd().split(os.sep)[-1]
-if len(arguments) < 3:
-    prompt = "Name of the bot? (default: " + main_name + "):"
-    bot_name = str(input(prompt))
-    if len(bot_name) == 0:
-        bot_name = main_name
-else:
-    bot_name = ' '.join(arguments[2:])
 
-prompt = "Name of the Creator:"
-user_name = str(input(prompt))
-if len(user_name) == 0:
-    user_name = "wizard_user"
+user_name = "akshay"
+print("Your ozz account is used for accessing the GUI admin panel")
+print("The admin panel lets you add/remove channels, access analytics and other services")
+answer = str(input("Do you have an account on Ozz.ai?[Y/N]:"))
 
-#Setup Password
-prompt = "Set a password for admin panel (typing hidden):"
-password = str(getpass.getpass(prompt))
+answer = answer.lower()
+flag = True
 
-user_name_email = user_name.replace(" ","")
-time_email = str(time.time()).replace(".","")
-email = "admin" + time_email + user_name_email + "@ozz.ai"
+while flag:
+    if answer == 'y' or answer == 'yes' or answer == 'yeah' or answer == 'yup':
+        email = str(input("Email:"))
+        password = str(getpass.getpass('Password:'))
 
-url = "https://api.ozz.ai/users"
-headers = {
-'content-type': "application/json",
-'cache-control': "no-cache"
-}
-payload = {
-    "name":user_name,
-    "email":email,
-    "password":password
-}
-response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
-print(response.text)
+        url = "https://api.ozz.ai/auth"
+        headers = {
+        'content-type': "application/json",
+        'cache-control': "no-cache"
+        }
+        payload = {
+            "email":email,
+            "password":password
+        }
+        
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
 
-response_token = json.loads(response.text)['token']
-super_secret = "hdh%&D*%^^%$8767VHGSDFT5$%SD$658"
+        response = json.loads(response.text)
 
-data = jwt.decode(response_token,super_secret,algorithms=['HS256'])
+        if 'email' in response:
+            print("\nNo account exists with that email. Try again")
+        elif 'password' in response:
+            print("\nInvalid password. Try Again")
+        elif 'success' in response and response['success'] == 'true':
+            flag = False
+    else:
+        #Setup Email
+        prompt = "Email for admin panel:"
+        email = str(input(prompt))
+        #Setup Password
+        prompt = "Password for admin panel (typing hidden):"
+        password = str(getpass.getpass(prompt))
 
-user_id = data["id"]
+        url = "https://api.ozz.ai/users"
+        headers = {
+        'content-type': "application/json",
+        'cache-control': "no-cache"
+        }
+        payload = {
+            "name":user_name,
+            "email":email,
+            "password":password
+        }
+        print("Setting up admin account....")
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
 
-token = "kfkshjfsd"
-nlp_name = "api"
+        response = json.loads(response.text)
 
-url = "https://api.ozz.ai/bots"
-headers = {
-    'content-type': "application/json",
-    'cache-control': "no-cache",
-    'authorization': 'Bearer ' + response_token
-}
-payload = {
-    "user_id":user_id,
-    "name":bot_name,
-    "platform":"wizard",
-    "app_secret":token,
-    "nlp_platform":nlp_name,
-    "webhook":""
-}
+        if 'email' in response:
+            print("\n\nAn email with that account already exists")
+            
+            while(True):
+                result = input('Do you want to use the existing account or create a new one? \n 1) Use Existing 2) Create New \n Please choose 1 or 2:')
+                try:
+                    result = int(result)
+                except ValueError:
+                    continue
+                if result == 1:
+                    flag = False
+                    break
+                else:
+                    break
+        else:
+            flag = False
+            response_token = response['token']
+            super_secret = "hdh%&D*%^^%$8767VHGSDFT5$%SD$658"
 
-response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
-print(response.text)
+            data = jwt.decode(response_token,super_secret,algorithms=['HS256'])
+
+            user_id = data["id"]
+            print(user_id)
